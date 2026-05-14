58ab1ad22dcc9e36de15b20133db6da24aff206c
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2026-05-15 00:13:41 +0200

    Practice pyramid and number patterns using nested loops in C

--- 02_Loops/06_basic_pyramid.c
+++ 02_Loops/06_basic_pyramid.c
@@ -1,11 +1,30 @@
 /*
-1. Expected Output:
+1. Centered Pyramid with stars:
+Expected Output:
         *
        * *
       * * *
      * * * *
     * * * * *
     
+2. Centered Pyramid with numbers
+Expected Output:
+
+        1
+       1 2
+      1 2 3
+     1 2 3 4
+    1 2 3 4 5
+    
+3. Inverted Pyramid with stars
+Expected Output:
+
+*********
+ *******
+  *****
+   ***
+    *
+
 */
 
 #include <stdio.h>
@@ -15,7 +34,8 @@ int main()
     int total_rows = 5;
     int row, column;
     
-    printf("\n\nTask 1 :\n\n");
+    // Task 1 : Centered Pyramid with stars :
+    printf("\n\nTask 1 : Centered Pyramid with stars :\n\n");
     
     // Outer for loop controls the total number of rows
     for(row = 1; row <= total_rows; row++)
@@ -35,6 +55,51 @@ int main()
         }
         
         printf("\n");
+    }
+    
+    // Task 2: Centered Pyramid with numbers
+    printf("\n\n Task 2: Centered Pyramid with numbers : \n\n");
+    
+    // Outer loop controls the total number of rows
+    for(row = 1; row <= total_rows; row++)
+    {
+        
+        // 1st Inner loop controls iterations of spaces in each row
+        for(column = 1; column <= total_rows - row; column++)
+        {
+            printf(" ");
+        }
+        
+        // 2nd inner loop controls iterations of numbers on each row
+        for(column = 1; column <= row; column++)
+        {
+            // Column numbers gets printed with space as suffix
+            printf("%d ", column);
+        }
+        printf("\n");
+    }
+  
+    // Inverted Pyramid with stars
+    printf("\n\nTask 3: Inverted Pyramid with stars :\n\n");
+    
+    // Outer loop controls total nbumber of rows
+    for(row = 1; row <= total_rows; row++)
+    {
+        // 1st inner loop controls number of leading spaces in each row
+        for(column = 1; column <= row - 1; column++)
+        {
+            printf(" ");
+        }
+        
+        // 2nd inner loop controls iterations of stars on each row
+        
+        for(column = 1; column <= 2 * (total_rows - row) + 1; column++)
+        {
+            printf("*");       
+        }
+        
+        printf("\n");
+        
     }
     
     return 0; // Upon successful execution
